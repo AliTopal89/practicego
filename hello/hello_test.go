685964ffe8c1aca684300efcebedf645fd7a1801
alitopaloglu <ali_t@gwmail.gwu.edu>
2019-07-02 23:37:13 -0400

    Constants and assertions

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -5,15 +5,33 @@
 // things like `t.Fail()` when you want to fail
 
 // f in Errorf stands for `format`
+
+// Added assertion into a function. This reduces duplication 
+// and improves readability of our tests. In Go you can declare functions 
+// inside other functions and assign them to variables.
 package main
 
 import "testing"
 
 func TestHello(t *testing.T) {
-    got := Hello("Ali")
-    want := "Hello, Ali"
 
-    if got != want {
-        t.Errorf("got '%s' want '%s'", got, want)
+    assertCorrectMessage := func(t *testing.T, got, want string) {
+        t.Helper()
+        if got != want {
+            t.Errorf("got '%s' want '%s'", got, want)
+        }
     }
+
+    t.Run("saying hello to people", func(t *testing.T) {
+        got := Hello("Ali")
+        want := "Hello, Ali"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("empty string defaults to 'World'", func(t *testing.T) {
+        got := Hello("")
+        want := "Hello, World"
+        assertCorrectMessage(t, got, want)
+    })
+
 }
