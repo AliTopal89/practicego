7112edf77b41c00f5800ac67359def860801be55
alitopaloglu <ali_t@gwmail.gwu.edu>
2019-07-08 23:02:05 -0400

    Adder test with example

--- integers/adder.go
+++ integers/adder.go
@@ -1,3 +1,4 @@
+// https://github.com/golang/go/wiki/CodeReviewComments#named-result-parameters
 package integers
 
 func Add(x, y int) int {
--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,9 +1,15 @@
 // using %d vs %s. That's because we want it 
 // to print an integer rather than a string.
 
+/*
+Godoc examples are snippets of Go code 
+that are displayed as package documentation 
+and that are verified by running them as tests.
+*/
 package integers
 
 import "testing"
+import	"fmt"
 
 func TestAdder(t *testing.T) {
     sum := Add(2, 2)
@@ -13,3 +19,15 @@ func TestAdder(t *testing.T) {
         t.Errorf("expected '%d' but got '%d'", expected, sum)
     }
 }
+
+func ExampleAdd() {
+    sum := Add(1, 5)
+    fmt.Println(sum)
+    // Output: 6
+}
+
+/*
+Please note that the example function will not 
+be executed if you remove the comment "//Output: 6". 
+Although the function will be compiled, it won't be executed.
+*/
