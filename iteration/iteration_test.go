118998403d047e593d1ffbb34590db041b54ad03
alitopaloglu <ali_t@gwmail.gwu.edu>
2019-07-16 22:54:25 -0400

    Play around with benchmark

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -12,3 +12,15 @@ func TestRepeat(t *testing.T) {
         t.Errorf("expected '%s' but got '%s'", expected, repeated)
     }
 }
+
+/* 
+When the benchmark code is executed, 
+it runs b.N times and measures how long it takes.
+*/
+
+func BenchmarkRepeat(b *testing.B) {
+    b.ResetTimer()
+    for i := 0; i < b.N; i++ {
+        Repeat("aaaaaaaaaaaa")  
+    }
+}
