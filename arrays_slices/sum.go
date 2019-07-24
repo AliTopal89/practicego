98a052f7c459c26d5b5bdf136159175102f31622
alitopaloglu <ali_t@gwmail.gwu.edu>
2019-07-24 11:28:09 -0400

    reflect deep equal and variadic functions

--- arrays_slices/sum.go
+++ arrays_slices/sum.go
@@ -1,5 +1,5 @@
 /* 
- Range lets you iterate over an array. 
+Range lets you iterate over an array. 
  Every time it is called it returns two values, 
  the index and the value. We are choosing to 
  ignore the index value by using _ blank identifier
@@ -14,4 +14,23 @@ func Sum(numbers []int) int {
         sum += number
     }
     return sum
+}
+
+/*
+Variadic functions can be called with 
+any number of trailing arguments. 
+For example, fmt.Println is a common variadic function.
+
+https://gobyexample.com/variadic-functions
+*/
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+    sums := make([]int, lengthOfNumbers)
+
+    for i, numbers := range numbersToSum {
+        sums[i] = Sum(numbers)
+    }
+
+    return sums
 }
