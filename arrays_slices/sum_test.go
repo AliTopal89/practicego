ef415ffea5fda4d61ea00a8b19f12b397e0ee7d3
alitopaloglu <ali_t@gwmail.gwu.edu>
2019-08-09 20:58:56 -0400

    refactor and test how to slice slices

--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -47,11 +47,28 @@ func TestSum(t *testing.T) {
 
 // The tail of a collection is all the items apart from the first one (the "head")
 func TestSumAllTails(t *testing.T) {
-
-    got := SumAllTails([]int{1,2}, []int{0,9})
-    want := []int{2, 9}
-
-    if !reflect.DeepEqual(got, want){
-        t.Errorf("got %v want %v", got, want)
+    checkSums := func(t *testing.T, got, want []int) {
+        t.Helper()
+        if !reflect.DeepEqual(got, want) {
+            t.Errorf("got %v want %v", got, want)
+        }
     }
+
+    t.Run("make the sums of some slices", func(t *testing.T){
+        got := SumAllTails([]int{1,2}, []int{0,9})
+        want := []int{2, 9}
+        checkSums(t, got, want)
+    })
+
+    t.Run("safely sum empty slices", func(t *testing.T){
+        got := SumAllTails([]int{}, []int{3, 4, 5})
+        want := []int{0, 9}
+        checkSums(t, got, want)
+    })
+    
+	t.Run("makes sums of some other slices", func(t *testing.T) {
+		got := SumAllTails([]int{1,2}, []int{1, 2, 3})
+		want := []int{2, 5}
+		checkSums(t, got, want)
+	})
 }
