2d9150b166bf7b3dd74a2f3dddf8bbd1ed1b332b
alitopaloglu <ali_t@gwmail.gwu.edu>
2019-09-05 23:46:44 -0400

    Method is a function with a receiver

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,7 +1,15 @@
 //A struct is a sequence of named elements, 
 //called fields, each of which has a name and a type.
 
+/* A method is a function with a receiver.
+A method declaration binds an identifier, the method name, 
+to a method, and associates the method with the receiver's base type.
+*/
+
 package structs
+import (
+	"math"
+)
 
 // Defining a struct type 
 type Rectangle struct {
@@ -9,10 +17,24 @@ type Rectangle struct {
     Height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-   return 2 *(rectangle.Height + rectangle.Width)	
+// func (receiverName RecieverType) MethodName(args)
+// you get your referene to methods data via the 'receiverName' variable.
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Height * rectangle.Width
+func(r Rectangle) Perimeter() float64 {
+	return 2*(r.Width + r.Height)
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+   return 2 *(rectangle.Height + rectangle.Width)	
 }
