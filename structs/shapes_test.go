c1b4f6f781974ceb7013ce2fb220beafb625249b
alitopaloglu <ali_t@gwmail.gwu.edu>
2019-09-08 23:30:31 -0400

    syntax ate my time couldn't learn anything

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -1,3 +1,11 @@
+
+/*
+created an interface with "checkArea"
+interfaces alllow to make functions 
+that can be used with different types and 
+create highly-independent code while still 
+maintaining the same type
+*/
 package structs
 
 import (
@@ -16,33 +24,29 @@ func TestPerimeter(t *testing.T) {
 // Methods are called by invoking them on an instance of a particular type
 
  //test cases that can be tested in the same manner.
-func TestArea(t *testing.T) {
+ func TestArea(t *testing.T) {
 
    areaTests := []struct {
-       shape Shape
-       want  float64
+       name    string
+       shape   Shape
+       hasArea float64
    }{
-      // assertions of truth about shapes and their areas
-      {shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
-      {shape: Circle{Radius: 10}, want: 314.1592653589793},
-      {shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+       {name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+       {name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+       {name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
    }
-// The blank identifier "_," provides a way to ignore 
-// left-hand side values in an assignment.
 
    for _, tt := range areaTests {
-       got := tt.shape.Area()
-       if got != tt.want {
-           t.Errorf("got %.2f want %.2f", got, tt.want)
-       }
+       // using tt.name from the case to use it as the `t.Run` test name
+       t.Run(tt.name, func(t *testing.T) {
+           got := tt.shape.Area()
+           if got != tt.hasArea {
+               t.Errorf("%#v got %.2f want %.2f", tt.shape, got, tt.hasArea)
+           }
+       })
+
    }
 
 }
 
-/*
-created an interface with "checkArea"
-interfaces alllow to make functions 
-that can be used with different types and 
-create highly-independent code while still 
-maintaining the same type
-*/
+
