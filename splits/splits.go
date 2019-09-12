61d68f4283f2119a7c491221ecfd48d64a45715d
alitopaloglu <ali_t@gwmail.gwu.edu>
2019-09-11 23:34:23 -0400

    understanding table driven tests better

---
+++ splits/splits.go
@@ -0,0 +1,21 @@
+package split
+ 
+import (
+    "strings"
+)
+
+/*
+Split slices s into all substrings separated by sep and returns a slice of
+the substrings between those separators.
+
+If s does not contain sep and sep is not empty, Split returns a
+slice of length 1 whose only element is s.
+
+If sep is empty, Split splits after each UTF-8 sequence. If both s
+and sep are empty, Split returns an empty slice.
+
+It is equivalent to SplitN with a count of -1.
+*/
+func Split(s, sep string) []string {
+	return strings.Split(s, sep)
+}
