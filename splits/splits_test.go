ffacd277b79a5e04bcc1819ae8399b5465995984
alitopaloglu <ali_t@gwmail.gwu.edu>
2019-09-11 23:38:19 -0400

    check deep equal prefered way

--- splits/splits_test.go
+++ splits/splits_test.go
@@ -1,14 +1,15 @@
 package split
 
 import (
-    "reflect"
-    "testing"
+	"testing"
+	"reflect"
 )
 
 func TestSplit(t *testing.T) {
-    got := Split("a/b/c", "/")
-    want := []string{"a", "b", "c"}
-    if !reflect.DeepEqual(want, got) {
-         t.Fatalf("expected: %v, got: %v", want, got)
-    }
+	got := Split("a/b/c", "/")
+	want := []string{"a","b","c"}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("want: %v, got: %v", want, got)
+	}
 }
