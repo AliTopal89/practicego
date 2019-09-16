e2af6ff0c02aef164e7f8645d7cf31a018791850
alitopaloglu <ali_t@gwmail.gwu.edu>
2019-09-16 00:09:04 -0400

    expand more with table driven tests and when to use it

--- splits/splits_test.go
+++ splits/splits_test.go
@@ -47,3 +47,44 @@ func TestSplitNoSep(t *testing.T){
 		t.Fatalf("want: %v, got: %v", want, got)
 	}
 }
+
+/*
+  To set up all the inputs and expected outputs and 
+  feed them to a single test harness, Its time 
+  to introduce table driven testing. 
+ 
+  We can even use an anonymous 
+  struct literal to reduce the boilerplate
+*/
+func TdTestSplit(t *testing.T){
+	tests := []struct{
+		input string
+		s string //seperation string
+		want []string
+
+	}{
+		{input: "a/b/c", s: "/", want: []string{"a", "b", "c"}},
+		{input: "a/b/c", s: ",", want: []string{"a/b/c"}},
+		{input: "abc", s: "/", want: []string{"abc"}},
+	}
+	
+	/*  
+	   _, ~ It avoids having to declare all the variables 
+	   for the returns values. You can see it in a loop too.
+
+	   If you only need the second item in the range (the value), 
+	   use the blank identifier, an underscore, to discard the first:
+
+	   'sum := 0
+		for _, value := range array {
+    		sum += value
+		}'
+	*/
+
+	for _, tc := range tests { // tc - test cases
+		got := Split(tc.input, tc.s)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Fatalf("want: %v, got : %v", tc.want, got)
+		}	
+	}
+}
