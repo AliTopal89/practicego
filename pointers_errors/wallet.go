2de83416563562d50e4e633588e07715610df556
alitopaloglu <ali_t@gwmail.gwu.edu>
2020-01-11 18:01:08 -0500

    write a TDD wallet withdrawal

--- pointers_errors/wallet.go
+++ pointers_errors/wallet.go
@@ -4,20 +4,40 @@ import (
 	"fmt"
 )
 
+type Bitcoin int 
+
+// type Myname original type so that our struct is more descriptives
+
 type Wallet struct {
-	balance int
-	//balance variable in our struct to store the state
- }
+	balance Bitcoin
+}
+//balance variable in our struct to store the state
+
+ type Stringer interface {
+	String() string
+}
+// implement Stringer interface on Bitcoin
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
 
 // remember methods
 // func (receiverName RecieverType) MethodName(args)
 
-func(w *Wallet) Deposit(amount int) {
+func(w *Wallet) Deposit(amount Bitcoin) {
 	//*Wallet ~ pointer to a wallet
 	fmt.Printf("origin of balance in Deposit method is %v \n", &w.balance)
 	w.balance += amount
 }
 
-func(w *Wallet) Balance() int {
+func(w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func(w *Wallet) Withdraw(amount Bitcoin) Bitcoin{
+	fmt.Printf("origin of balance in Withdraw method is %v \n", &w.balance)
+	w.balance -= amount
+
 	return w.balance
 }
--- pointers_errors/wallet_test.go
+++ pointers_errors/wallet_test.go
@@ -6,17 +6,38 @@ import (
 )
 
 func TestWallet(t *testing.T)  {
-	wallet := Wallet{}
-	wallet.Deposit(10)
+	t.Run("Deposit", func(t *testing.T){
+	    wallet := Wallet{}
+	    wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
+		got := wallet.Balance()
 
-	fmt.Printf("origin of balance in test is %v \n", &wallet.balance)
-													//&myVal
-	want := 10
-	
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+		fmt.Printf("origin of balance in test is %v \n", &wallet.balance)
+														//&myVal - origin of that bit of memory
+		want := Bitcoin(10)
+		
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+
+	})
+
+	t. Run("Withdraw", func(t *testing.T){
+		 wallet := Wallet{balance: Bitcoin(20)}
+
+		 wallet.Withdraw(Bitcoin(10))
+
+		 got := wallet.Balance()
+
+		 fmt.Printf("origin of withdrawal balance in test is %v \n", &wallet.balance)
+
+		 want := Bitcoin(10)
+
+		 if got != want {
+			 t.Errorf("got %s want %s", got, want)
+		 }
+	})
 }
 // in our very secure wallet we don't want to expose our inner state to the rest of the world
+// by doing "Bitcoin(999)" its making a new type to declare methods on.
+// update our test format strings so they will use String() instead.
