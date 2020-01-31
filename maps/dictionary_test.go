50d37c2ac39fbd6265da91a6ab238338363c9737
alitopaloglu <ali_t@gwmail.gwu.edu>
2020-01-31 11:16:18 -0500

    Logf text will be printed only if the test fails or the -test.v flag is set

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -9,14 +9,14 @@ func TestSearch(t *testing.T) {
 	// t of type *testing.T is your "hook" into the testing framework 
 	//so you can do things like t.Fail() when you want to fail.
 	dictionary := Dictionary{"test": "this is just a test"}
-
+    t.Logf("testing known word")
     t.Run("known word", func(t *testing.T) {
         got, _ := dictionary.Search("test")
         want := "this is just a test"
 
         assertStrings(t, got, want)
     })
-
+    t.Logf("testing unknown word")
 	t.Run("unknown word", func(t *testing.T) {
         _, err := dictionary.Search("unknown")
         want := "could not find the word you were looking for"
