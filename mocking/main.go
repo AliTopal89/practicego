00e93ca3d9771f6e65b910d5c8e85162ef6e544e
alitopaloglu <ali_t@gwmail.gwu.edu>
2020-03-04 21:25:24 -0500

    Add sleep 1 second

--- mocking/main.go
+++ mocking/main.go
@@ -4,15 +4,22 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
+
+const finalWord = "Go!"
 
 // use general purpose interface instead of *bytes.Buffer
 // Use a for loop counting backwards with i--
 func Countdown(out io.Writer) {
 	for i := 3; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		fmt.Printf("Current Time: %v\n", time.Now())
+		time.Sleep(1 * time.Second)
+		fmt.Printf("Current Time: %v\n", time.Now())
 	}
-	fmt.Fprintf(out, "Go!")
+
+	fmt.Fprintf(out, finalWord)
 
 }
 
