efb35428ea8574d1b450ab4a0adfd12258a622c2
alitopaloglu <ali_t@gwmail.gwu.edu>
2020-03-31 23:50:31 -0400

    benchmark test for website checking

---
+++ concurrency/check_websites_benchmark_test.go
@@ -0,0 +1,29 @@
+package concurrency
+
+import (
+	"testing"
+	"time"
+)
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
+
+/*
+  The benchmark tests CheckWebsites using a slice of one hundred
+  urls and uses a new fake implementation of "WebsiteChecker".
+  "slowStubWebsiteChecker" is deliberately slow. It uses "time.Sleep"
+  to wait exactly twenty milliseconds and then it returns true
+*/
