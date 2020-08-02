a1db0c6690e28ee67f469c611af740fd4a81cfa1
alitopaloglu <ali_t@gwmail.gwu.edu>
2020-08-02 18:36:15 -0400

    Get string passed to fn corrected with reflect package ValueOf

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -1,8 +1,24 @@
 package main
 
+import "reflect"
+
+/* The reflect package has a function ValueOf which
+   returns us a Value of a given variable. This has ways
+   for us to inspect a value, including its fields which
+   we use on the next line.
+*/
 func walk(x interface{}, fn func(input string)) {
-	fn("Hi This is a string blah blah")
+	val := reflect.ValueOf(x)
+	field := val.Field(0)
+	fn(field.String())
 }
+
+/* We look at the first and only field, there may be
+   no fields at all which would cause a panic,We then call
+   `String()` which returns the underlying value as a string
+   but we know it would be wrong if the field was something
+   other than a string.
+*/
 
 func main() {
 
