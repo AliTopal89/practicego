c8467b7596fa5a52654d49624b0c8e6ea89bbae5
alitopaloglu <ali_t@gwmail.gwu.edu>
2020-08-30 23:52:55 -0400

    With a larger number of commands example

--- cli/main.go
+++ cli/main.go
@@ -7,30 +7,63 @@ import (
 	cli "github.com/jawher/mow.cli"
 )
 
+// Global options available to any of the commands
+var filename *string
+
 func main() {
-	// create an app
-	app := cli.App("cp", "Copy files around")
+	app := cli.App("vault", "Password Keeper")
 
-	// Here's what differentiates mow.cli from other CLI libraries:
-	// This line is not just for help message generation.
-	// It also validates the call to reject calls with less than 2 arguments
-	// and split the arguments between SRC or DST
-	app.Spec = "[-r] SRC... DST"
+	// Define our top-level global options
+	filename = app.StringOpt("f file", os.Getenv("HOME")+"/.safe", "Path to safe")
 
-	var (
-		// declare the -r flag as a boolean flag
-		recursive = app.BoolOpt("r recursive", false, "Copy files recursively")
-		// declare the SRC argument as a multi-string argument
-		src = app.StringsArg("SRC", nil, "Source files to copy")
-		// declare the DST argument as a single string (string slice) arguments
-		dst = app.StringArg("DST", "", "Destination where to copy files to")
-	)
+	// Define our command structure for usage like this:
+	app.Command("list", "list accounts", cmdList)
+	app.Command("creds", "display account credentials", cmdCreds)
+	app.Command("config", "manage accounts", func(config *cli.Cmd) {
+		config.Command("list", "list accounts", cmdList)
+		config.Command("add", "add an account", cmdAdd)
+		config.Command("remove", "remove an account(s)", cmdRemove)
+	})
 
-	// Specify the action to execute when the app is invoked correctly
-	app.Action = func() {
-		fmt.Printf("Copying %v to %s [recursively: %v]\n", *src, *dst, *recursive)
-	}
-
-	// Invoke the app passing in os.Args
 	app.Run(os.Args)
+}
+
+// Sample use: vault list OR vault config list
+func cmdList(cmd *cli.Cmd) {
+	cmd.Action = func() {
+		fmt.Printf("list the contents of the safe here")
+	}
+}
+
+// Sample use: vault creds reddit.com
+func cmdCreds(cmd *cli.Cmd) {
+	cmd.Spec = "ACCOUNT"
+	account := cmd.StringArg("ACCOUNT", "", "Name of account")
+	cmd.Action = func() {
+		fmt.Printf("display account info for %s\n", *account)
+	}
+}
+
+// Sample use: vault config add reddit.com -u username -p password
+func cmdAdd(cmd *cli.Cmd) {
+	cmd.Spec = "ACCOUNT [ -u=<username> ] [ -p=<password> ]"
+	var (
+		account  = cmd.StringArg("ACCOUNT", "", "Account name")
+		username = cmd.StringOpt("u username", "admin", "Account username")
+		password = cmd.StringOpt("p password", "admin", "Account password")
+	)
+	cmd.Action = func() {
+		fmt.Printf("Adding account %s:%s@%s", *username, *password, *account)
+	}
+}
+
+// Sample use: vault config remove reddit.com twitter.com
+func cmdRemove(cmd *cli.Cmd) {
+	cmd.Spec = "ACCOUNT..."
+	var (
+		accounts = cmd.StringsArg("ACCOUNT", nil, "Account names to remove")
+	)
+	cmd.Action = func() {
+		fmt.Printf("Deleting accounts: %v", *accounts)
+	}
 }
