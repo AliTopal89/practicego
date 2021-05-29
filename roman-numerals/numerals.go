ddd7d4948664288e4a9583ff7e52839af03db506
alitopaloglu <ali_t@gwmail.gwu.edu>
2021-05-29 16:43:00 -0400

    Refactor and Test converting roman to number

--- roman-numerals/numerals.go
+++ roman-numerals/numerals.go
@@ -11,7 +11,18 @@ type RomanNumeral struct {
 }
 
 var symboltonum = []RomanNumeral{
-	{1000, "M"}, {500, "D"}, {100, "C"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"},
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
 }
 
 func ConvertToRoman(roman int) string {
@@ -25,4 +36,8 @@ func ConvertToRoman(roman int) string {
 	}
 	fmt.Println(result.String())
 	return result.String()
+}
+
+func ConvertToNum(num string) int {
+	return 1
 }
--- roman-numerals/numerals_test.go
+++ roman-numerals/numerals_test.go
@@ -1,12 +1,15 @@
 package numerals
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestRomanNumerals(t *testing.T) {
-	cases := []struct {
+var (
+	cases = []struct {
 		Description string
 		Num         int
-		Want        string
+		Roman       string
 	}{
 		{"1 gets converted to I", 1, "I"},
 		{"2 gets converted to II", 2, "II"},
@@ -22,13 +25,28 @@ func TestRomanNumerals(t *testing.T) {
 		{"47 gets converted to XLVII", 47, "XLVII"},
 		{"49 gets converted to XLIX", 49, "XLIX"},
 		{"50 gets converted to L", 50, "L"},
+		{"1984 gets converted to MCMLXXXIV", 1984, "MCMLXXXIV"},
+		{"2021 gets converted to MMXXI", 2021, "MMXXI"},
 	}
+)
 
+func TestConvertingToRoman(t *testing.T) {
 	for _, test := range cases {
 		t.Run(test.Description, func(t *testing.T) {
 			got := ConvertToRoman(test.Num)
-			if got != test.Want {
-				t.Errorf("got %q, want %q", got, test.Want)
+			if got != test.Roman {
+				t.Errorf("got %q, want %q", got, test.Roman)
+			}
+		})
+	}
+}
+
+func TestConvertingtoNumerals(t *testing.T) {
+	for _, test := range cases[:1] {
+		t.Run(fmt.Sprintf("%q gets converted to %d", test.Roman, test.Num), func(t *testing.T) {
+			got := ConvertToNum(test.Roman)
+			if got != test.Num {
+				t.Errorf("got %d want %d", got, test.Num)
 			}
 		})
 	}
