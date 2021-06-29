ca8cde8a73c6bb42a435d747b22a8c2cebb3eaca
alitopaloglu <ali_t@gwmail.gwu.edu>
2021-06-29 00:55:15 -0400

    Refactor with few constants

--- clockface/clockface.go
+++ clockface/clockface.go
@@ -12,14 +12,18 @@ type Point struct {
 	Y float64
 }
 
+const secondHandLength = 90
+const clockCentreX = 150
+const clockCentreY = 150
+
 // SecondHand is the unit vector of the second hand of an analogue clock at time `t`
 // represented as a Point.
 func SecondHand(t time.Time) Point {
 	p := SecondHandPoint(t)
-	p = Point{p.X * 90, p.Y * 90} // scale, it to the length of hand
-	p = Point{p.X, -p.Y}          // flip, it over X axis to account for the
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale, it to the length of hand
+	p = Point{p.X, -p.Y}                                      // flip, it over X axis to account for the
 	// SVG having an origin in the top left hand corner
-	p = Point{p.X + 150, p.Y + 150} // translate, it the rgiht position
+	p = Point{p.X + clockCentreX, p.Y + clockCentreY} // translate, it the right position
 	// (so that its coming from origin 150,150)
 	return p
 }
