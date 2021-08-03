38f38eb863818739e49822b4cc60f406bef6e150
alitopaloglu <ali_t@gwmail.gwu.edu>
2021-08-03 15:19:21 -0400

    add containsLine helper function

--- clockface/clockface_acceptance_test.go
+++ clockface/clockface_acceptance_test.go
@@ -35,13 +35,21 @@ type Line struct {
 	Y2 float64 `xml:"y2,attr"`
 }
 
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
+}
+func testName(c time.Time) string {
+	return c.Format("15:04:05")
+}
+
 func TestSVGWriterAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-
 	b := bytes.Buffer{}
+
 	clockface.SVGWriter(&b, tm)
 
 	svg := SVG{}
+
 	xml.Unmarshal(b.Bytes(), &svg)
 
 	want := Line{150, 150, 150, 60}
@@ -52,9 +60,48 @@ func TestSVGWriterAtMidnight(t *testing.T) {
 		}
 	}
 
-	t.Errorf("Expected to find the second hand line %+v in the SVG Lines output %v", want, svg.Line)
-
+	t.Errorf("Expected to find the second hand line %+v, in the SVG lines %+v", want, svg.Line)
 }
+
+func TestSVGWriterSecondHandSVG(t *testing.T) {
+	cases := []struct {
+		time time.Time
+		line Line
+	}{
+		{
+			simpleTime(0, 0, 0),
+			Line{150, 150, 150, 60},
+		},
+		{
+			simpleTime(0, 0, 30),
+			Line{150, 150, 150, 240},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			b := bytes.Buffer{}
+			clockface.SVGWriter(&b, c.time)
+
+			svg := SVG{}
+			xml.Unmarshal(b.Bytes(), &svg)
+
+			if !containsLine(c.line, svg.Line) {
+				t.Errorf("Expected to find the second hand line %+v, in the SVG lines %+v", c.line, svg.Line)
+			}
+		})
+	}
+}
+
+func containsLine(l Line, ls []Line) bool {
+	for _, line := range ls {
+		if line == l {
+			return true
+		}
+	}
+	return false
+}
+
 func TestSecondHandAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
