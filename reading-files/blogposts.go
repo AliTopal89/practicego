55e6b9795400796d4377de3d9354af2c896067be
alitopaloglu <ali_t@gwmail.gwu.edu>
2021-11-17 01:20:13 -0500

    Inject a failing fs.FS test-double to make fs.ReadDir return an error

--- reading-files/blogposts.go
+++ reading-files/blogposts.go
@@ -1,19 +1,46 @@
 package blogposts
 
-import "io/fs"
+import (
+	"io"
+	"io/fs"
+)
 
 type Post struct {
+	Title string
 }
 
 // Package fs defines basic interfaces to a file system.
 
 // The FS interface is the minimum implementation required of the file system.
 // A file system may implement additional interfaces,
-func NewPostsFromFS(fileSystem fs.FS) []Post {
-	dir, _ := fs.ReadDir(fileSystem, ".")
-	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".")
+	if err != nil {
+		return nil, err
 	}
-	return posts
+	var posts []Post
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f)
+		if err != nil {
+			return nil, err //todo: needs clarification, should we totally fail if one file fails? or just ignore?
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	post := Post{Title: string(postData)[7:]}
+	return post, nil
 }
--- reading-files/blogposts_test.go
+++ reading-files/blogposts_test.go
@@ -7,13 +7,21 @@ import (
 	blogposts "github.com/quii/learn-go-with-tests/reading-files"
 )
 
+type Post struct {
+	Title string
+}
+
 func TestNewBlogPosts(t *testing.T) {
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+		"hello world.md":  {Data: []byte("Title: Post 1")},
+		"hello-world2.md": {Data: []byte("Title: Post 2")},
 	}
 
-	posts := blogposts.NewPostsFromFS(fs)
+	posts, err := blogposts.NewPostsFromFS(fs)
+
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	if len(posts) != len(fs) {
 		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
