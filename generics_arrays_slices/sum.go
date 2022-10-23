29c18f77ed8c4ae647035f2ebdc50c1cc9d32d96
alitopaloglu <ali_t@gwmail.gwu.edu>
2022-10-23 17:40:18 -0400

    Collection, accumulating function, initial value, return single value with reduce pattern

--- generics_arrays_slices/sum.go
+++ generics_arrays_slices/sum.go
@@ -1,24 +1,31 @@
 package arrays
 
-func Sum(numbers []int) int {
-    sum := 0
-    for _, number := range numbers {
-        sum += number
-    }
-    return sum
+// Reduce captures the essence of the pattern, it's a function that takes a collection,
+// an accumulating function, an initial value, and returns a single value.
+func Reduce[A any](collection []A, accumulator func(A, A) A, initialValue A) A {
+	var result = initialValue
+	for _, x := range collection {
+		result = accumulator(result, x)
+	}
+	return result
 }
 
-// take from 1 to the end" with numbers[1:]
-func SumAllTails(numbersToSum ...[]int) []int {
-    var sums []int
-    for _, numbers := range numbersToSum {
-        if len (numbers) == 0 {
-            sums = append(sums, 0)
-        } else {
-            tail := numbers[1:]
-            sums = append(sums, Sum(tail))
-        }
-    }
+// Sum calculates the total from a slice of numbers.
+func Sum(numbers []int) int {
+	add := func(acc, x int) int { return acc + x }
+	return Reduce(numbers, add, 0)
+}
 
-    return sums
+// SumAllTails calculates the sums of all but the first number given a collection of slices.
+func SumAllTails(numbers ...[]int) []int {
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
+		} else {
+			tail := x[1:]
+			return append(acc, Sum(tail))
+		}
+	}
+
+	return Reduce(numbers, sumTail, []int{})
 }
--- generics_arrays_slices/sum_test.go
+++ generics_arrays_slices/sum_test.go
@@ -1,73 +1,100 @@
 // %v	the value in a default format
 // format strings: https://golang.org/pkg/fmt/
 
-/* 
-For example, the type [4]int represents 
+/*
+For example, the type [4]int represents
 an array of four integer values layed out sequentially.
 
-Slicing does not copy the slice's data. 
-It creates a new slice value that points to the original array. 
-This makes slice operations as efficient as manipulating array indices. 
-Therefore, modifying the elements (not the slice itself) of a re-slice 
+Slicing does not copy the slice's data.
+It creates a new slice value that points to the original array.
+This makes slice operations as efficient as manipulating array indices.
+Therefore, modifying the elements (not the slice itself) of a re-slice
 modifies the elements of the original slice:
 */
 package arrays
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 )
 
 func TestSum(t *testing.T) {
 
-    t.Run("collection of 5 numbers", func(t *testing.T) {
-        numbers := []int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-        got := Sum(numbers)
-        want := 15
+		got := Sum(numbers)
+		want := 15
 
-        if got != want {
-            t.Errorf("got %d want %d given, %v", got, want, numbers)
-        }
-    })
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 
-    t.Run("collection of any size", func(t *testing.T) {
-        numbers := []int{1, 2, 3}
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 
-        got := Sum(numbers)
-        want := 6
+		got := Sum(numbers)
+		want := 6
 
-        if got != want {
-            t.Errorf("got %d want %d given, %v", got, want, numbers)
-        }
-    })
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 
+}
+
+// %+v - any kind value
+// example with interface that doesn't check for
+func AssertEqual(t *testing.T, got, want interface{}) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
 }
 
 // The tail of a collection is all the items apart from the first one (the "head")
 func TestSumAllTails(t *testing.T) {
-    checkSums := func(t *testing.T, got, want []int) {
-        t.Helper()
-        if !reflect.DeepEqual(got, want) {
-            t.Errorf("got %v want %v", got, want)
-        }
-    }
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
 
-    t.Run("make the sums of some slices", func(t *testing.T){
-        got := SumAllTails([]int{1,2}, []int{0,9})
-        want := []int{2, 9}
-        checkSums(t, got, want)
-    })
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
 
-    t.Run("safely sum empty slices", func(t *testing.T){
-        got := SumAllTails([]int{}, []int{3, 4, 5})
-        want := []int{0, 9}
-        checkSums(t, got, want)
-    })
-    
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+
 	t.Run("makes sums of some other slices", func(t *testing.T) {
-		got := SumAllTails([]int{1,2}, []int{1, 2, 3})
+		got := SumAllTails([]int{1, 2}, []int{1, 2, 3})
 		want := []int{2, 5}
 		checkSums(t, got, want)
+	})
+}
+
+func TestReduce(t *testing.T) {
+	t.Run("multiplication of all elements", func(t *testing.T) {
+		multiply := func(x, y int) int {
+			return x * y
+		}
+
+		AssertEqual(t, Reduce([]int{1, 2, 3}, multiply, 1), 6)
+	})
+
+	t.Run("concatenate strings", func(t *testing.T) {
+		concatenate := func(x, y string) string {
+			return x + y
+		}
+
+		AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
 	})
 }
