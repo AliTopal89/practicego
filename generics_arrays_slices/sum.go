3f83cd6c202280fb272a77a94d4cb792d7309595
alitopaloglu <ali_t@gwmail.gwu.edu>
2022-11-13 19:02:15 -0500

    allows people to Reduce from a collection of A into a B. In our case from Transaction to float64.

--- generics_arrays_slices/sum.go
+++ generics_arrays_slices/sum.go
@@ -7,7 +7,7 @@ import (
 
 // Reduce captures the essence of the pattern, it's a function that takes a collection,
 // an accumulating function, an initial value, and returns a single value.
-func Reduce[A any](collection []A, accumulator func(A, A) A, initialValue A) A {
+func Reduce[A, B any](collection []A, accumulator func(B, A) B, initialValue B) B {
 	var result = initialValue
 	for _, x := range collection {
 		result = accumulator(result, x)
