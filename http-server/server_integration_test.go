b132006a8e78c84f416dbe5fc1e3aa1eb90d3232
alitopaloglu <ali_t@gwmail.gwu.edu>
2023-01-22 15:16:58 -0500

    New In Memory player store to intialize the store and add integration tests

---
+++ http-server/server_integration_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+// trying to integrate with: InMemoryPlayerStore and PlayerServer.
+// store a variable response
+// because we are going to try and get the player's score.
+func TestRecordingWinsAndRetrievingThem(t *testing.T) {
+	store := NewInMemoryPlayerStore()
+	server := PlayerServer{store}
+	player := "Mojojo"
+
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+
+	response := httptest.NewRecorder()
+	server.ServeHTTP(response, NewGetScoreRequest(player))
+	assertStatus(t, response.Code, http.StatusOK)
+
+	assertResponseBody(t, response.Body.String(), "3")
+}
